033fd5d68adfb97897047cd746806d2949e6e81b
3tg-3645 <eliabe.nekket@gmail.com>
2022-05-04 16:26:41 -0300

    test

---
+++ global javascript.js
@@ -0,0 +1,10 @@
+var nb1=document.getElementById('nbr1')
+var rsult=document.getElementById('result')
+var n=nbr1.value
+var tm=new Date()
+var y=tm.getFullYear
+var calc=tm-y
+function clicar(){
+    rsult.innerHTML=`${tm.getFullYear()}`
+
+}
